6c9a9e9e264d60bf5542df164f316001889dac2d
hasanmd91 <hasanmd1991@hotmail.com>
2022-11-23 13:58:02 +0200

    build: fetch data

--- src/Components/Pokelist.js
+++ src/Components/Pokelist.js
@@ -1,23 +1,34 @@
 import React from "react";
+import { Component } from "react";
+import { json } from "react-router-dom";
 import Card from "./Card";
 import style from "./pokelist.module.css";
 
-const Pokelist = () => {
-  return (
-    <div className={style.container}>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-    </div>
-  );
-};
+export default class Pokelist extends Component {
+  state = { data: [], isLoading: false };
 
-export default Pokelist;
+  componentDidMount() {
+    this.setState({ isLoading: true });
+
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({ data: data.results, isLoading: false });
+      });
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return <p> Loading....</p>;
+    }
+
+    console.log(this.state.data);
+    return (
+      <div className={style.container}>
+        {this.state.data.map((card) => (
+          <Card name={card.name} key={card.name} />
+        ))}
+      </div>
+    );
+  }
+}
